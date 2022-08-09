21648ce74c7b22fb52fed300cfedbb017575de3e
tharushimd <110925589+tharushimd@users.noreply.github.com>
2022-08-09 23:40:28 +0530

    Create global.js

---
+++ global.js
@@ -0,0 +1,24 @@
+//global
+
+// ⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈⧈
+//▮QUEEN THARU MD BOT 2022 ®️ALL RIGHTS RESERVED
+//▮
+//▮FORK AND DON'T FORGET TO GIVE A STAR
+//▮
+//▮THIS SOFTWARE IS UNDER UZ COPYRIGHT
+//▮
+//▮REPORT ABUSE OF THIS SOFTWARE
+//▮http://shewon.ml/
+//▮WHATSAPP US : +94 78 499 7828
+//▮YOUTUBE CHANNELL: https://youtube.com/c/slcartooninstudio
+//▮
+//╰▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+//
+//┏━━━━━━━━━━━━━━━━━━━━━━━━━
+//┃THIS SOFTWARE INCLUDES 
+//┃SOME ENCRYPTED FILES
+//┃
+//┃THANKS FOR CHOOSING QUEEN THARU-BOT
+//┃THANKS TO DIKA ARDNT
+//┗━━━━━━━━━━━━━━━━━━━━━━━━━
+//
